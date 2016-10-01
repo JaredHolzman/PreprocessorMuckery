136cd6a7eb5a09d8a72ecd9988d4614b9885182b
Jared Holzman <jared@holzman.io>
2016-10-01 18:53:17 -0400

    Add fee and alarm functions

--- confused.c
+++ confused.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 // %:define as() printf
+%:define alarm(c, o, n, f, u, s, e, d)  f ## o ## e
+%:define amaze(c, o, n, f, u, s, e, d)  o ## u ## e  ## f
+%:define fie(c, h, u, m, p) amaze(s, c, e, n, a, r, i, o)
+%:define fee(e, l, k) alarm(u, n, z, i, p, p, e, d)
 %:define baz() return
 %:define foo(a, b) a ## b
 %:define $ printf
-%:define fum %>
-%:define fee() int
-%:define amaze(c, o, n, f, u, s, e, d)  o ## u ## e  ## f
-%:define fie(c, h, u, m, p) amaze(s, c, e, n, a, r, i, o)
 
-fee()fie(m, i, x, u, p)()foo(<, %)$("Confused?\n");baz()0;fum
+fee(t, h, e)fie(m, i, x, u, p)()foo(<, %)$("Confused?\n");baz()0;%>
