a61ecdd0f8380aeaa2ec58c47be1a6e0c2cf976d
Jared Holzman <jared@holzman.io>
2016-10-04 14:14:39 -0400

    Fix to reuse stupify macro

--- nextLevel/superConfused.c
+++ nextLevel/superConfused.c
@@ -7,9 +7,8 @@
 %:define impress(c, h, u, m, p) amaze(s, c, e, n, a, r, i, o)
 %:define perplex(e, l, k) alarm(u, n, z, i, p, p, e, d)
 %:define shock(c,l,a,m,p) bewilder(p,e,r,m,e,a,t,e)
-%:define startle $-$
-%:define stagger() <%
-%:define stupify() %>
+%:define stagger $-$
+%:define stupify() %
 %:define electrify(g,o,a,l) affect(r,i,n,g,t,a,i,l)
 
-perplex(t, h, e)impress(m, i, x, u, p)()stagger()electrify(f,l,i,p)("Confused?\n");shock(m,o,u,r,n)(perplex(t,a,r))startle;stupify()
+perplex(t, h, e)impress(m, i, x, u, p)()<stupify()electrify(f,l,i,p)("Confused?\n");shock(m,o,u,r,n)(perplex(t,a,r))startle;stupify>
